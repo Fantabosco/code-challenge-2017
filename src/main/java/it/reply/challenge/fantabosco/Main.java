2fbc7a06f7ec3540e47e4dd6b81e7d2680a06aaa
rpt <rpt@altervista.org>
2017-09-28 12:58:42 +0200

    First commit

---
+++ src/main/java/it/reply/challenge/fantabosco/Main.java
@@ -0,0 +1,14 @@
+package it.reply.challenge.fantabosco;
+
+public class Main {
+	
+	private Main(){
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		// TODO Auto-generated method stub
+		
+	}
+
+}
