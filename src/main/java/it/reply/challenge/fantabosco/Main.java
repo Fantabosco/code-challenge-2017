f8dcec37f70c7885a7429f8b765a2af0f050b06a
rpt <rpt@altervista.org>
2017-10-10 16:09:53 +0200

    start

--- src/main/java/it/reply/challenge/fantabosco/Main.java
+++ src/main/java/it/reply/challenge/fantabosco/Main.java
@@ -1,12 +1,31 @@
 package it.reply.challenge.fantabosco;
 
+import it.reply.challenge.fantabosco.model.Event;
+import it.reply.challenge.fantabosco.model.Room;
+
+import java.util.List;
+
 public class Main {
+
+	private static String FILE_1 = "data_50000_10.in";
+	private static String FILE_2 = "data_50000_100.in";
+	private static String FILE_3 = "data_5000_3.in";
+	private static String FILE_4 = "data_5000_10.in";
 	
-	private Main(){
-	}
+	List<Event> events;
+	List<Room> rooms;
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		readFile(FILE_1);
+		
+		//TODO algoritmo
+
+	}
+
+	private static void readFile(String file) {
+		//TODO by giova
+		
+
 	}
 
 }
